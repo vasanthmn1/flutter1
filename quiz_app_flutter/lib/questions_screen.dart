54f020bfdac8b8676080f2da40052842fb5d76c3
vasanth <mnvasanth4@gmail.com>
2023-11-01 07:45:50 +0530

    some change 2 project

--- quiz_app_flutter/lib/questions_screen.dart
+++ quiz_app_flutter/lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/data/questions.dart';
+import 'package:quiz_app_flutter/reusable/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,18 +12,36 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text("The Questions"),
-        const SizedBox(
-          height: 30,
-        ),
-        ElevatedButton(onPressed: () {}, child: Text("Click")),
-        ElevatedButton(onPressed: () {}, child: Text("Click")),
-        ElevatedButton(onPressed: () {}, child: Text("Click")),
-        ElevatedButton(onPressed: () {}, child: Text("Click")),
-      ]),
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                currentQuestion.text,
+                // TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ...currentQuestion.getShuffledAnswers().map((e) {
+                return AnswerButton(
+                  answerText: e,
+                  onTap: () {},
+                );
+              })
+              // AnswerButton(answerText: "Answer Text1 ...", onTap: () {}),
+              // AnswerButton(answerText: "Answer Text2 ...", onTap: () {}),
+              // AnswerButton(answerText: "Answer Text3 ...", onTap: () {}),
+              // AnswerButton(answerText: "Answer Text4 ...", onTap: () {}),
+            ]),
+      ),
     );
   }
 }
---
+++ quiz_app_flutter/lib/reusable/answer_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    this.onTap,
+  });
+
+  final String answerText;
+  final void Function()? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Color.fromARGB(255, 171, 2, 238),
+            foregroundColor: Colors.white,
+            padding: EdgeInsets.all(10),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10))),
+        child: Text(answerText),
+      ),
+    );
+  }
+}
