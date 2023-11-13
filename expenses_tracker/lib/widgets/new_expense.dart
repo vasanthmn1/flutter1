2aee4dd4d64288964387d56675d92425241bc6a2
vasanth <mnvasanth4@gmail.com>
2023-11-13 21:24:02 +0530

    adding date picker

--- expenses_tracker/lib/widgets/new_expense.dart
+++ expenses_tracker/lib/widgets/new_expense.dart
@@ -1,4 +1,6 @@
+import 'package:expenses_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -11,6 +13,21 @@ class _NewExpenseState extends State<NewExpense> {
   // control text input
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final fistDate = DateTime(now.year - 1);
+
+    final pickDate = await showDatePicker(
+        context: context, initialDate: now, firstDate: fistDate, lastDate: now);
+
+    setState(() {
+      _selectedDate = pickDate;
+    });
+
+    //
+  }
+
   @override
   void dispose() {
     super.dispose();
@@ -30,12 +47,34 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text("Title")),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            maxLength: 50,
-            decoration:
-                const InputDecoration(label: Text("Amount"), prefixText: '\$'),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                      label: Text("Amount"), prefixText: '\$'),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(_selectedDate == null
+                      ? 'Mo Date Selected'
+                      : formatter.format(_selectedDate!)),
+                  IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calculate))
+                ],
+              ))
+            ],
           ),
           Row(
             children: [
